cac6f27b4f1350387e2116180d4a50ba34dedf92
Sue Sui <suesui117@gmail.com>
2023-05-20 21:04:12 -0400

    AllEmployeesViews.js added the Delete button, same as AllTasksView.js, need to update DeleteEmployee function in AllEmployeesContainer.js

--- src/components/views/AllEmployeesView.js
+++ src/components/views/AllEmployeesView.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const AllEmployeesView = (props) => {
+  let {employees, deleteEmployee} = props;
   if (!props.allEmployees.length) {
     return <div>There are no employees.
       <Link to={`/newemployee`}>
@@ -20,7 +21,9 @@ const AllEmployeesView = (props) => {
             <h1>{name}</h1>
           </Link>
           <p>{employee.department}</p>
+          <button onClick={ () => deleteEmployee(employee.id)}>Delete</button>
         </div>
+
         );
 
       })}
