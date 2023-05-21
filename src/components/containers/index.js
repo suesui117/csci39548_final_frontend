027f26c76490986d4ac3dd31ff56c73cbd968b78
Sue Sui <suesui117@gmail.com>
2023-05-20 22:47:17 -0400

    updataing ./containers/index.js file to include the new file: EditEmployeeContainers.js

--- src/components/containers/index.js
+++ src/components/containers/index.js
@@ -1,14 +1,18 @@
 // Barrel file for container components (makes it easier to import into App.js)
 export { default as HomePageContainer } from "./HomePageContainer";
 
+// ALL EMPLOYEES && ALL TASKS (INCLUDING DELETE)
 export { default as AllEmployeesContainer } from "./AllEmployeesContainer";
 export { default as AllTasksContainer } from "./AllTasksContainer";
 
+// SINGLE EMPLOYEE && SINGLE TASK
 export { default as EmployeeContainer } from "./EmployeeContainer";
 export { default as TaskContainer } from "./TaskContainer";
 
+// ADD NEW EMPLOYEE && TASK
 export { default as NewEmployeeContainer } from "./NewEmployeeContainer";
 export { default as NewTaskContainer } from "./NewTaskContainer";
 
+// EDIT EMPLOYEE && TASK
 export { default as EditTaskContainer } from "./EditTaskContainer";
 export { default as EditEmployeeContainer } from "./EditEmployeeContainer";
