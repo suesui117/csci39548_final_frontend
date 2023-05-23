d2a8f7973cb61e3450274321015da571bc6a7bdc
Sue Sui <suesui117@gmail.com>
2023-05-23 15:52:49 -0400

    activated the link to employee on single task page

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -13,28 +13,34 @@ const TaskView = (props) => {
       
       <h3 className="task-priority">
         <span style={{ fontSize: '20px', marginRight: '5px' }}>Priority:</span>
-        {task.priority}
+        <span style={{ color: 'orange' }}>{task.priority}</span>
       </h3>
 
       <h3 className="task-priority">
         <span style={{ fontSize: '20px', marginRight: '5px' }}>Completion status:</span>
-        {completionStatus}
+        <span style={{ color: 'orange' }}>{completionStatus}</span>
       </h3>
 
       {task.employee ? (
-        <h2 style={{ color: 'orange' }} className="task-employee">
-          {task.employee.firstname + " " + task.employee.lastname}
-        </h2>
-      ) : (
-        <h2 style={{ color: 'orange' }} className="task-employee">
+        <h3>
+          <span style={{ color: 'pink' }}>Assigned Employee: </span>
+          <Link style={{ color: 'orange' }} to={`/employee/${task.employee.id}`}> {task.employee.firstname + " " + task.employee.lastname} </Link>
+        </h3>
+              ) : (
+        <h3 style={{ color: 'orange' }}>
           Staff
-        </h2>
+        </h3>
       )}
 
+      <div>
+        {Array(7).fill(<br />)}
+      </div>
+      <h3>
       <Link className="task-button" to={`/edittask/${task.id}`}>Edit task information</Link>
       <br/>
       <br/>
       <Link className="task-button" to={`/tasks`}>View all tasks</Link>
+      </h3>
     </div>
   );
 };
