d9b105aecee748a74882fbae8ebdd280caf3b656
Sue Sui <suesui117@gmail.com>
2023-05-23 15:24:19 -0400

    modified taskView.js/css to display single task's priority level and completion status

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
-import "./TaskView.css"
+import "./TaskView.css";
 
 const TaskView = (props) => {
   const { task } = props;
+  const completionStatus = task.isComplete ? "Completed" : "Incomplete";
+
   return (
     <div className="task-view">
       <h1 className="task-name">{task.description}</h1>
       
-      {task.employee ? <h2 style={{ color: 'orange' }} className="task-employee"> {task.employee.firstname + " " + task.employee.lastname}</h2>: <h2 style={{ color: 'orange' }} className="task-employee">staff</h2>}
-      
-      <Link className="task-button" to={`/edittask/${task.id}`}> Edit task information </Link>
+      <h3 className="task-priority">
+        <span style={{ fontSize: '20px', marginRight: '5px' }}>Priority:</span>
+        {task.priority}
+      </h3>
+
+      <h3 className="task-priority">
+        <span style={{ fontSize: '20px', marginRight: '5px' }}>Completion status:</span>
+        {completionStatus}
+      </h3>
+
+      {task.employee ? (
+        <h2 style={{ color: 'orange' }} className="task-employee">
+          {task.employee.firstname + " " + task.employee.lastname}
+        </h2>
+      ) : (
+        <h2 style={{ color: 'orange' }} className="task-employee">
+          Staff
+        </h2>
+      )}
+
+      <Link className="task-button" to={`/edittask/${task.id}`}>Edit task information</Link>
       <br/>
-      <Link className="task-button" to={`/tasks`}> View all tasks </Link>
-    
+      <br/>
+      <Link className="task-button" to={`/tasks`}>View all tasks</Link>
     </div>
   );
-
 };
 
 export default TaskView;
